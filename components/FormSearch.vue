89e3643539f9bcc259735850e70124e97db149f0
T <t@T.local>
2018-06-23 14:11:21 -0400

    added simple component for form search

---
+++ components/FormSearch.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="form-search">
+      <input v-model="term" placeholder="Search movie" />
+      <button @click="onSubmit">Search</button>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    onSubmit: {
+      type: Function,
+    }
+  },
+  data() {
+    return {
+      term: '',
+    };
+  },
+}
+</script>
+
+<style>
+.form-search {
+  display: flex;
+  align-items: center;
+}
+</style>
