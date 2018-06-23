e34647e80adedefdf8ffa5a5c3140a7494f07585
T <t@T.local>
2018-06-23 14:10:48 -0400

    added results store

---
+++ store/results.js
@@ -0,0 +1,58 @@
+import * as api from '~/assets/api'; // eslint-disable-line import/extensions
+
+const GET = '/GET';
+const GET_SUCCESS = '/GET/SUCCESS';
+const GET_ERROR = '/GET/ERROR';
+
+const initialState = () => ({
+  loading: undefined,
+  error: undefined,
+  term: undefined,
+  data: undefined,
+});
+
+export const getters = {
+  data: state => state.data,
+  term: state => state.term,
+  error: state => state.error,
+  loading: state => state.loading,
+};
+
+export const actions = {
+  getList: ({ commit }, term) => {
+    commit(GET, term);
+
+    return api.getContentListByTerm(term)
+      .then(data => commit(GET_SUCCESS, data))
+      .catch(error => commit(GET_ERROR, error));
+  },
+}
+export const mutations = {
+  [GET]: (store, term) => {
+    const mutation = {
+      loading: true,
+      error: false,
+      term,
+    };
+
+    Object.assign(store, mutation);
+  },
+  [GET_SUCCESS]: (store, data) => {
+    const mutation = {
+      loading: false,
+      data,
+    };
+
+    Object.assign(store, mutation);
+  },
+  [GET_ERROR]: (store, error) => {
+    const mutation = {
+      loading: false,
+      error,
+    };
+
+    Object.assign(store, mutation);
+  },
+};
+
+export const state = initialState;
